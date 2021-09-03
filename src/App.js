4701d56f0b614bda9e66b37f50e3eb9fc0bd242b
Gabriel <gabriel.gabrielyea@gmail.com>
2021-09-03 11:35:27 -0500

    Add config files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-unused-vars */
+import { Provider } from 'react-redux';
+import MissionContainer from './components/missions/missionContainer/MissionContainer';
+import Loader from './components/loader/Loader';
+import store from './redux/configureStore';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Loader />
+      </div>
+    </Provider>
   );
 }
 
