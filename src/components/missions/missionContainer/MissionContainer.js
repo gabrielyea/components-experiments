d86b785eb6f13c46213deef9a08ac7aba288d39c
Gabriel <gabriel.gabrielyea@gmail.com>
2021-09-03 17:38:07 -0500

    Refactor missions so they work with loader

--- src/components/missions/missionContainer/MissionContainer.js
+++ src/components/missions/missionContainer/MissionContainer.js
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { motion } from 'framer-motion';
 import { fetchAllMissions } from '../../../redux/slice/componentSlice';
+import { setLoading } from '../../../redux/slice/loaderSlice';
 import Mission from '../mission/Mission';
 import styles from './missionContainerStyle.module.scss';
 
@@ -17,15 +18,16 @@ const container = {
   },
 };
 
-const MissionContainer = () => {
-  // const dispatch = useDispatch();
+const MissionContainer = ({ toggle }) => {
+  const dispatch = useDispatch();
   const allMissions = useSelector((state) => state.components.entities);
 
-  // useEffect(() => {
-  //   if (allMissions.length === 0) {
-  //     dispatch(fetchAllMissions());
-  //   }
-  // }, [dispatch]);
+  useEffect(() => {
+    if (allMissions.length === 0) {
+      // dispatch(fetchAllMissions());
+      // toggle();
+    }
+  }, []);
 
   const createMissions = (list) => list.map((mission) => (
     <Mission
---
+++ src/pages/Missions.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-unused-vars */
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import MissionContainer from '../components/missions/missionContainer/MissionContainer';
+import Loader from '../components/loader/Loader';
+import { fetchAllMissions } from '../redux/slice/componentSlice';
+
+const Mission = () => {
+  const selector = useSelector((state) => state.components.loading);
+  const list = useSelector((state) => state.components.entities);
+  const dispatch = useDispatch();
+
+  const handleCall = () => {
+    dispatch(fetchAllMissions());
+  };
+
+  return (
+    <>
+      <Loader
+        loading={selector}
+        list={list}
+        callDispatch={handleCall}
+      >
+        <MissionContainer />
+      </Loader>
+    </>
+  );
+};
+export default Mission;
